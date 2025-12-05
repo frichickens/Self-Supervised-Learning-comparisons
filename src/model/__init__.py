38ad52425105928e0a60dbd9b4ce9a5334dcd8d2
thecoder2004 <nguyentu11102004@gmail.com>
2025-12-05 20:06:26 +0700

    init

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,10 +1,10 @@
 from .mobilenet import MobileNetV1
-from .resnet import ResNet
+from .resnet import ResNet50
 
 def create_model(opt):
     if opt['model'] == 'mobilenet':
         return MobileNetV1(opt['out_nc'])
     elif opt['model'] == 'resnet':
-        return ResNet(opt['out_nc'])
+        return ResNet50(opt['out_nc'])
     else:
-        raise NotImplementedError('Model [{:s}] is not recognized.'.format(opt['network_G']['which_model_G']))
+        raise NotImplementedError('Model [{:s}] is not recognized.'.format(opt['model']))
