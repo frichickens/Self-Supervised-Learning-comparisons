7b06f0d69360ea44187eb8d68b21898a0e8e086c
thecoder2004 <nguyentu11102004@gmail.com>
2025-12-06 14:11:54 +0700

    finalize pipeline

--- src/train.py
+++ src/train.py
@@ -11,114 +11,153 @@ import argparse
 from dotenv import load_dotenv
 from huggingface_hub import login
 from utils.utils import *
+from tqdm import tqdm
 
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-opt', type=str, help='Path to option YAML file.')
+parser.add_argument('-root', type=str, default=None, choices=['.'])
+args = parser.parse_args()
+opt = option.parse(args.opt, root=args.root)
+opt = option.dict_to_nonedict(opt)
+model = create_model(opt)
+train_set, train_loader, valid_set, valid_loader, test_set, test_loader = create_ds(opt)
+
+
+train_hypers = opt['hyperparameters']
+checkpoint_path = opt['checkpoint_path']
+
+loss_func = nn.CrossEntropyLoss()
+optimizer = create_optimizer(model.parameters(), train_hypers)
+lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, train_hypers['epochs'], train_hypers['eta_min'])
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+model = load_checkpoint(model, opt['pretrain_path'])
+model.to(device)
+
 
 def train():
-    for epoch in range(num_epochs):
+    best_acc = float('-inf')
+    global_step = 0
+    
+    for epoch in range(train_hypers['epochs']):
         total_train_loss = 0
-        total_valid_loss = 0
-        
+        total_preds = []
+        total_gts = []
         #Training process
-        for i, data in enumerate(train_loader):  
-            #Move tensors to the configured device
-            images, labels = data
+        for batch in tqdm(train_loader, total=len(train_loader)):
             optimizer.zero_grad()
-                
-            #Forward pass
-            outputs = model(images.to(device))
-            loss = loss_func(outputs, labels.to(device))
+            imgs, gts = batch
+            batch_size = imgs.shape[0]
             
-            total_train_loss += loss.item() * images.size(0)
+            imgs = imgs.to(device)
+            gts = gts.to(device)
+            
+            pred = model(imgs)
+            loss = loss_func(pred, gts)
+            
+            total_preds.append(pred.detach().cpu())
+            total_gts.append(gts.detach().cpu())
+            
+            total_train_loss += loss.item() * batch_size
+            
             #Backward and optimize
             loss.backward()
             optimizer.step()
             
-            #Calculate training accuracy
-            _, predicted = torch.max(outputs.data, 1)
-            correct = (predicted == labels.to(device)).sum().item()
-            train_acc = float(correct)/float(images.to(device).shape[0])
+            global_step += 1
+            if global_step % opt['validate_freq'] == 0:
+                eval_loss, eval_metrics = validate()
+                print(f"[EVAL] Step {global_step}|{eval_loss}")
+                wandb.log({f"eval_step_{k}": v for k, v in eval_metrics.items()})
+                
+                if eval_metrics['accuracy'] > best_acc:
+                    best_acc = eval_metrics['accuracy']
+                    save_checkpoint(model, os.path.join(checkpoint_path, str(opt['model'] + "_" + timestamp),'_best_model.pth'))
+                
         
-        print ('Epoch [{}/{}], Loss: {:.4f}'.format(epoch+1, num_epochs, total_train_loss/10500))
-    
-    
+        total_preds = torch.cat(total_preds, dim=0)
+        total_gts = torch.cat(total_gts, dim=0)
+        
+        train_loss = total_train_loss / len(train_set)
+        print(f"[Train] Epoch {epoch}|{train_loss}")
+        train_metrics = calculate_metrics(total_preds, total_gts, num_classes=10)
+        wandb.log({f"train_epoch_{k}": v for k, v in train_metrics.items()})
+        
+        lr_scheduler.step()
     
 def validate():
-    model=model.eval()
-    correct = 0
-    total = 0
-    for images, labels in valid_loader:
-        images = images.to(device)
-        labels = labels.to(device)
-        outputs = model(images)
+    model.eval()
+    
+    total_valid_loss = 0
+    total_preds = []
+    total_gts = []
+    for batch in tqdm(valid_loader, total=len(valid_loader)):  
+        imgs, gts = batch
+        batch_size = imgs.shape[0]
         
-        total_valid_loss += loss_func(outputs, labels).item() * labels.size(0)
+        imgs = imgs.to(device)
+        gts = gts.to(device)
         
-        _, predicted = torch.max(outputs.data, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
-        del images, labels, outputs
+        pred = model(imgs)
+        loss = loss_func(pred, gts)
         
-    # calculate_metrics()    
-    total_train_loss = total_train_loss/10500
-    total_valid_loss = total_valid_loss/3000
-
+        total_preds.append(pred.detach().cpu())
+        total_gts.append(gts.detach().cpu())
+        
+        total_valid_loss += loss.item() * batch_size
+    
+    total_preds = torch.cat(total_preds, dim=0)
+    total_gts = torch.cat(total_gts, dim=0)
+    
+    eval_loss = total_valid_loss / len(valid_set)
+    eval_metrics = calculate_metrics(total_preds, total_gts, num_classes=10)
+    return eval_loss, eval_metrics
 
 
 def test():
-    model=model.eval()
-    correct = 0
-    total = 0
-    for images, labels in test_loader:
-        images = images.to(device)
-        labels = labels.to(device)
-        outputs = model(images)
-        _, predicted = torch.max(outputs.data, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
-        del images, labels, outputs
-
-    #calculate_metrics()
+    model.eval()
+    
+    total_test_loss = 0
+    total_preds = []
+    total_gts = []
+    for batch in tqdm(test_loader, total=len(test_loader)):
+        imgs, gts = batch
+        batch_size = imgs.shape[0]
+        
+        imgs = imgs.to(device)
+        gts = gts.to(device)
+        
+        pred = model(imgs)
+        loss = loss_func(pred, gts)
+        total_preds.append(pred.detach().cpu())
+        total_gts.append(gts.detach().cpu())
+        total_test_loss += loss.item() * batch_size
+    
+    total_preds = torch.cat(total_preds, dim=0)
+    total_gts = torch.cat(total_gts, dim=0)
+    test_loss = total_test_loss / len(test_set)
+    test_metrics = calculate_metrics(total_preds, total_gts, num_classes=10)
+    return test_loss, test_metrics
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-opt', type=str, help='Path to option YAML file.')
-    parser.add_argument('-root', type=str, default=None, choices=['.'])
-    args = parser.parse_args()
-    opt = option.parse(args.opt, root=args.root)
-    opt = option.dict_to_nonedict(opt)
-    model = create_model(opt)
-    train_set, train_loader, valid_set, valid_loader, test_set, test_loader = create_ds(opt)
-    model = load_checkpoint(model, opt['pretrain_path'])
-    
-    train_hypers = opt['hyperparameters']
-    num_epochs = 70
-    learning_rate = 0.006
 
-    #Loss and optimizer
-    loss_func = nn.CrossEntropyLoss()
-    optimizer = create_optimizer(model.parameters(), train_hypers)
-    lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, train_hypers['epochs'], train_hypers['eta_min'])
-    
-    if opt['use_wandb']:
-        load_dotenv()
-        wandb_key=os.getenv('WANDB_KEY')
-        timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-        wandb.login(key=wandb_key)
-        wandb.init(
-            project="SSL_Comparison",
-            name=str(opt['name'] + "- time stamp -" + timestamp))
+    load_dotenv()
+    wandb_key=os.getenv('WANDB_KEY')
+    timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    wandb.login(key=wandb_key)
+    wandb.init(
+        project="SSL_Comparison",
+        name=str(opt['model'] + "- time stamp -" + timestamp))
 
-        wandb.config.learning_rate = opt['hyperparameters']['lr']
-        wandb.config.epochs = opt['hyperparameters']['epochs']
-        wandb.config.beta1 = opt['hyperparameters']['beta1']
-        wandb.config.beta2 = opt['hyperparameters']['beta2']
-        wandb.config.eta_min = opt['hyperparameters']['eta_min']
-        wandb.config.seed = opt['hyperparameters']['seed']
+    wandb.config.learning_rate = opt['hyperparameters']['lr']
+    wandb.config.epochs = opt['hyperparameters']['epochs']
+    wandb.config.beta1 = opt['hyperparameters']['beta1']
+    wandb.config.beta2 = opt['hyperparameters']['beta2']
+    wandb.config.eta_min = opt['hyperparameters']['eta_min']
+    wandb.config.seed = opt['hyperparameters']['seed']
         
-    
     if opt['is_train']:
         train()
     elif opt['is_test']:
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,5 +1,17 @@
 from data import *
 import torch
+from sklearn.metrics import (
+    accuracy_score,
+    precision_score,
+    recall_score,
+    f1_score,
+    roc_auc_score,
+    average_precision_score,
+    label_binarize,
+)
+import numpy as np
+
+
 
 
 def create_ds(opt):
@@ -15,11 +27,15 @@ def create_ds(opt):
             test_loader = create_dataloader(test_set, dataset_opt, opt, None)
     return train_set, train_loader, valid_set, valid_loader, test_set, test_loader
 
+
+
 def load_checkpoint(model, checkpoint_path):
     checkpoint = torch.load(checkpoint_path)
     model.load_state_dict(checkpoint)
     print(f"Loaded checkpoint from {checkpoint_path}")
     return model
+
+
 
 def create_optimizer(params, opt):
     return torch.optim.Adam(
@@ -28,7 +44,51 @@ def create_optimizer(params, opt):
         weight_decay = opt['weight_decay'],
         
     )
-    
-    
-def calculate_metrics():
-    pass
+
+
+def save_checkpoint(model, checkpoint_path):
+    torch.save(model.state_dict(), checkpoint_path)
+    print(f"Saved checkpoint to {checkpoint_path}")
+
+
+def calculate_metrics(
+    logits: torch.Tensor,
+    labels: torch.Tensor,
+    num_classes: int = 10):
+    """
+    Compute classification metrics for multi-class 
+
+    Args:
+        logits: (B, C) raw scores from the model
+        labels: (B,) ground-truth class indices
+        num_classes: 10 for CIFAR-10
+
+    Returns:
+        dict with accuracy, precision, recall, f1, roc_auc, pr_auc (macro-averaged)
+    """
+    probs = torch.softmax(logits, dim=1).cpu().numpy()
+    preds = torch.argmax(logits, dim=1).cpu().numpy()
+    gts   = labels.cpu().numpy()
+
+    metrics = {
+        "accuracy" : accuracy_score(gts, preds),
+        "precision": precision_score(gts, preds, average='macro', zero_division=0),
+        "recall"   : recall_score(gts, preds, average='macro', zero_division=0),
+        "f1"       : f1_score(gts, preds, average='macro', zero_division=0),
+    }
+
+    gts_bin = label_binarize(gts, classes=range(num_classes))
+    if gts_bin.shape[1] == 1:                     # edge case (only 1 class in batch)
+        gts_bin = np.hstack((1 - gts_bin, gts_bin))
+
+    try:
+        metrics["roc_auc"] = roc_auc_score(gts_bin, probs, average='macro', multi_class='ovr')
+    except ValueError:
+        metrics["roc_auc"] = 0.0
+
+    try:
+        metrics["pr_auc"] = average_precision_score(gts_bin, probs, average='macro')
+    except ValueError:
+        metrics["pr_auc"] = 0.0
+
+    return metrics
