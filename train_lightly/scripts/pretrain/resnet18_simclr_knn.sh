af9c09d88b1996144d751ed9755cb134b13e4db7
thecoder2004 <nguyentu11102004@gmail.com>
2025-12-23 19:32:56 +0700

    Add scripts

---
+++ train_lightly/scripts/pretrain/resnet18_simclr_knn.sh
@@ -0,0 +1,3 @@
+export WANDB_API_KEY="9c233a1274e348c64884fa361aac455906cf6a0e"
+wandb login --relogin $WANDB_API_KEY
+python /mnt/disk4/baodq/Self-Supervised-Learning-comparisons/train_lightly/model/resnet18_dino_knn.py
